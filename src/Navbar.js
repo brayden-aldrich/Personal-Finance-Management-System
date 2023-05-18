8c2e717578656785fe4b91c856e910c0fce7af5e
brayden-aldrich <aldricbr@oregonstate.edu>
2023-05-18 11:32:49 -0700

    Initialization
    - Removed (most) unnecessary sample files that are created with the react app.
    - Made a simple navbar component to test out react

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+export default function Navbar() {
+  return (
+    <div id="navbar-div">
+        <h1>Personal Finance Management System</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Budgets</a></li>
+            <li><a href="#">Balance</a></li>
+
+        </ul>
+    </div>
+  )
+}
