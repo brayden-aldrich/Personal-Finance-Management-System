49aec434915137b6bd8865b3ad53202883c98172
brayden-aldrich <aldricbr@oregonstate.edu>
2023-05-18 12:05:55 -0700

    CSS and Navbar updates
    - added some css to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './Navbar.css'
 export default function Navbar() {
   return (
-    <div id="navbar-div">
-        <h1>Personal Finance Management System</h1>
-        <ul>
+
+    <div id="navbar">
+        <ul class="left">
             <li><a href="#">Home</a></li>
             <li><a href="#">Budgets</a></li>
             <li><a href="#">Balance</a></li>
-
+        </ul>
+        <ul class="right">
+                <li><a href="#">Login</a></li>
         </ul>
     </div>
   )
