ab53f3f7af3c529efa4217a48e10e3d40ded49e8
EthanN0089 <nixone@oregonstate.edu>
2023-06-01 16:05:47 -0700

    Start fix for Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,55 +1,59 @@
-//PLACEHOLDER
+// PLACEHOLDER
 
-// # Author: Daniel Mendes
-// # Date:   5/9/23
-// # Desc:   This is just a draft of how we could create and operate 
-// #         budget objects in the final project. This has basic JSON
-// #         file I/O and a general structure for a "Budget" Object
+// Author: Daniel Mendes
+// Date:   5/9/23
+// Desc:   This is just a draft of how we could create and operate 
+//         budget objects in the final project. This has basic JSON
+//         file I/O and a general structure for a "Budget" Object
 
-// import json
+//import json
 
-// class Budget:
-//     ##########################################
-//     ### BUDGET CLASS DATA MEMBERS
-//     ##########################################
-//     budgetName = ""
-//     expenseArray = []
+class Budget {
+//  ##########################################
+//  ### BUDGET CLASS DATA MEMBERS
+//  ##########################################
+    /** @type {string} */
+    name
 
-//     ##########################################
-//     ### BUDGET CLASS METHODS
-//     ##########################################
+    /** @type {Array} */
+    expenseArray
+
+//  ##########################################
+//  ### BUDGET CLASS METHODS
+//  ##########################################
     
-//     #Constructor just builds the object with 
-//     def __init__(self, name: str):
-//         self.budgetName = name
+//  #Constructor just builds the object with 
+    constructor(name) {
+        this.name = name
+        this.expenseArray = []
+    }
 
-//     def insertExpenseData(self, name: str, cost: int, date: str):
-//         #First, insert info about the expense into a temp dict
-//         expenseDict = {}
-//         expenseDict['expense_name'] = name
-//         expenseDict['expense_cost'] = cost
-//         expenseDict['expense_date'] = date
+    insertExpenseData(name, date, amount):
+        // #First, insert info about the expense into a temp dict
+        var expense = Expense(name, date, amount)
 
-//         #Then, insert it into the budget object's expense array
-//         self.expenseArray.append(expenseDict)
+        // #Then, insert it into the budget object's expense array
+        self.expenseArray.append(expenseDict)
 
-//     #Provides a lowercase, no-space version of the budget name
-//     #   Used to create temp filenames
-//     def simplifyName(self) -> str:
-//         simpleName = self.budgetName.replace(" ","").lower()
-//         return simpleName
+    // #Provides a lowercase, no-space version of the budget name
+    // #   Used to create temp filenames
+    def simplifyName(self) -> str:
+        simpleName = self.budgetName.replace(" ","").lower()
+        return simpleName
 
-//     #Exports the expense array to a json file
-//     def exportExpensesToJson(self):
-//         jsonFileName = self.simplifyName() + ".json"
+    // #Exports the expense array to a json file
+    def exportExpensesToJson(self):
+        jsonFileName = self.simplifyName() + ".json"
 
-//         with open("./" + jsonFileName, "w") as f:
-//             json.dump(self.expenseArray, f, indent = 4, separators=(",", ": "))
+        with open("./" + jsonFileName, "w") as f:
+            json.dump(self.expenseArray, f, indent = 4, separators=(",", ": "))
 
-//     #Imports a new expense array from a json file
-//     #   Note: Overwrites the previous expense array
-//     def importExpensesFromJson(self, filename: str):
-//         with open(filename, "r") as f:
-//             self.expenseArray = json.load(f)
+    // #Imports a new expense array from a json file
+    // #   Note: Overwrites the previous expense array
+    def importExpensesFromJson(self, filename: str):
+        with open(filename, "r") as f:
+            self.expenseArray = json.load(f)
 
-// export default Budget
+        }
+
+export default Budget
