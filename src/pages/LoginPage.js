fe303b8875847020f059365f9da6e72939987a3b
stevencaiOR <stevencaiOR@gmail.com>
2023-06-10 21:22:36 -0700

    created HTML structure for login page and added bulk of CSS for it. still in work in progress for now

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,23 @@
 import React from "react";
 //import EncDec from "../components/EncDec";
+import './LoginPage.scss';
+import '../App.scss';
 
 function LoginPage() {
     return(
         <>
-            <h1>
-            This is the login page
+            <h1 id="sign-in-header">
+            Sign into MyFinancePal
             </h1>
+            <div id="loginPanel">
+                <form>
+                    <label for="username" class="loginLabel">Username</label>
+                    <input id="username" class="loginInput" placeholder="Enter Username" required></input>
+                    <label for="password" class="loginLabel">Password</label>
+                    <input id="password" class="loginInput" placeholder="Enter Password" required></input>
+                    <input id="loginSubmit" class="loginInput" type="submit" value="Login"></input>
+                </form>
+            </div>
         </>
     );
 }
